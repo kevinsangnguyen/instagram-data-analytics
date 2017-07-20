033918c3f0ace712ed57632b531ccac3b73b4d2a
kevinsangnguyen <kevinsangnguyen@gmail.com>
2017-07-20 02:34:07 -0700

    changed port

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(express.static(path.join(__dirname, './client')));
 app.use('/scripts', express.static(__dirname + '/node_modules'));
 
 // require('./server/config/routes.js')(app);
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(8000, function() {
-  console.log('cool stuff on: 8000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
